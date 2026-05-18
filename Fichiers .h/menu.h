fee93864414366d9634db6bdb02fec12db8a19fa
spooky-rx <mathieu.bonheur44@gmail.com>
2026-05-18 19:29:35 +0200

    Delete Fichiers .h directory

--- Fichiers .h/menu.h
+++
@@ -1,7 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-#include <allegro5/allegro.h>
-#include "entites.h"
-#include "sons.h"
-void afficher_menu(ALLEGRO_DISPLAY *d, Sons *s);
-#endif
--- Fichiers .h/sons.h
+++
@@ -1,8 +0,0 @@
-#ifndef SONS_H
-#define SONS_H
-#include "entites.h"
-Sons charger_sons(void);
-void jouer_musique(Sons *s, ALLEGRO_SAMPLE *nouveau);
-void jouer_effet(ALLEGRO_SAMPLE *s);
-void liberer_sons(Sons *s);
-#endif
